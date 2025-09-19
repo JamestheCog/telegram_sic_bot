224f9d27d0e2d5b3930d0d1b633fb11d19d14ff3
JamestheCog <jcog196013@gmail.com>
2025-09-19 14:59:12 +0800

    Changed app's entry point for render

--- app.py
+++ app.py
@@ -1,24 +1,22 @@
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 from handlers.messages import respond, start_conversation, restart_conversation, display_help_menu
 import os, dotenv 
+from logger.bot_data import bot_data
 
 dotenv.load_dotenv()
 
-async def handle_message(update, context):
-    await update.message.reply_text(f"You said: {update.message.text}")
+application = Application.builder().token(os.getenv('TELEGRAM_BOT_TOKEN')).build()
 
-def main():
-    application = Application.builder().token(os.getenv('TELEGRAM_BOT_TOKEN')).build()
-
-    # Add handlers for commands and messages
-    application.add_handler(CommandHandler('start', start_conversation))
-    application.add_handler(CommandHandler('help', display_help_menu))
-    application.add_handler(CommandHandler('restart', restart_conversation))
-    application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, respond))
-
-    # Start the bot
-    print('running...')
-    application.run_polling()
+# Add handlers for commands and messages
+application.add_handler(CommandHandler('start', start_conversation))
+application.add_handler(CommandHandler('help', display_help_menu))
+application.add_handler(CommandHandler('restart', restart_conversation))
+application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, respond))
 
 if __name__ == "__main__":
-    main()
+    application.run_webhook(
+        listen = "0.0.0.0",
+        port = int(10000),  # Render default port
+        url_path = "webhook",
+        webhook_url = os.getenv('WEBHOOK_URL')
+    )
