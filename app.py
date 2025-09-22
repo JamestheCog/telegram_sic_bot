b2c0ffd874036193074093c2a6a702dd8a52c7ad
JamestheCog <jcog196013@gmail.com>
2025-09-22 09:55:11 +0800

    Added logging functionality
    A dummy file has also been included to amke user_data persist

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 from telegram.ext import Application, CommandHandler, MessageHandler, filters
 from handlers.messages import respond, start_conversation, restart_conversation, display_help_menu
-import os, dotenv 
+import os, dotenv, logging
 from logger.bot_data import bot_data
 
 dotenv.load_dotenv()
@@ -13,7 +13,16 @@ application.add_handler(CommandHandler('help', display_help_menu))
 application.add_handler(CommandHandler('restart', restart_conversation))
 application.add_handler(MessageHandler(filters.TEXT & ~filters.COMMAND, respond))
 
+# Enable logging for debugging purposes when needed:
+logging.basicConfig(
+    format = '%(asctime)s - %(name)s - %(levelname)s - %(message)s',
+    level = logging.INFO
+)
+logger = logging.getLogger(__name__)
+
 if __name__ == "__main__":
+    logger.info('Application started...')
+    logger.info(f"Application is running at: {os.getenv('WEBHOOK_URL')}")
     application.run_webhook(
         listen = "0.0.0.0",
         port = int(10000),  # Render default port
